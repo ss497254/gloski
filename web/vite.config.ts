66f73a927e2f124e9b48a39ac39eac1914cdec63
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2026-01-28 14:13:15 +0000

    feat: add manual chunking configuration for optimized build output

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,4 +10,25 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('@codemirror')) {
+            return 'vendor.code-editor'
+          } else if (id.includes('@xterm')) {
+            return 'vendor.xterm'
+          } else if (id.includes('@lezer')) {
+            return 'vendor.lezer'
+          } else if (id.includes('node_modules')) {
+            return 'vendor'
+          } else if (id.includes('src/features/')) {
+            const featureName = id.split('src/features/')[1].split('/')[0] ?? '[fix-me]'
+            return 'feature_' + featureName
+          }
+        },
+      },
+    },
+  },
 })
